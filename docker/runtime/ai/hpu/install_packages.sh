3d9015a6686c67ed1099ea7d80c6dc2b2180b515
Chen Haifeng <haifeng.chen@intel.com>
2023-09-04 16:56:11 +0800

    Gaudi: AI runtime image with PyTorch framework (#11)

---
+++ docker/runtime/ai/hpu/install_packages.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -ex
+
+pt_package_name="pytorch_modules-v${PT_VERSION}_${VERSION}_${REVISION}.tgz"
+os_string="ubuntu${OS_NUMBER}"
+pt_artifact_path="https://${ARTIFACTORY_URL}/artifactory/gaudi-pt-modules/${VERSION}/${REVISION}/pytorch/${os_string}"
+
+tmp_path=$(mktemp --directory)
+wget --no-verbose "${pt_artifact_path}/${pt_package_name}"
+tar -xf "${pt_package_name}" -C "${tmp_path}"/.
+pushd "${tmp_path}"
+./install.sh $VERSION $REVISION
+popd
+# cleanup
+rm -rf "${tmp_path}" "${pt_package_name}"
