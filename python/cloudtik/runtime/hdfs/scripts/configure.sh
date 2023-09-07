55f1ea364e098982164bb4abc4a066375d864f8f
Chen Haifeng <haifeng.chen@intel.com>
2023-09-07 12:07:20 +0800

    Runtime: handle HDFS case more robust for Mount runtime (#26)

--- python/cloudtik/runtime/hdfs/scripts/configure.sh
+++ python/cloudtik/runtime/hdfs/scripts/configure.sh
@@ -69,8 +69,8 @@ function update_hdfs_data_disks_config() {
     if [ -z "$hdfs_dn_dirs" ]; then
         hdfs_dn_dirs="${HADOOP_HOME}/data/dfs/dn"
     fi
-    sed -i "s!{%dfs.namenode.name.dir%}!${hdfs_nn_dirs}!g" `grep "{%dfs.namenode.name.dir%}" -rl ${output_dir}`
-    sed -i "s!{%dfs.datanode.data.dir%}!${hdfs_dn_dirs}!g" `grep "{%dfs.datanode.data.dir%}" -rl ${output_dir}`
+    sed -i "s!{%dfs.namenode.name.dir%}!${hdfs_nn_dirs}!g" ${HDFS_SITE_CONFIG}
+    sed -i "s!{%dfs.datanode.data.dir%}!${hdfs_dn_dirs}!g" ${HDFS_SITE_CONFIG}
 }
 
 function update_proxy_user_for_current_user() {
@@ -85,33 +85,49 @@ function update_proxy_user_for_current_user() {
         <name>hadoop.proxyuser.${CURRENT_SYSTEM_USER}.hosts</name>\n\
         <value>*</value>\n\
     </property>"
-        sed -i "s#{%hadoop.proxyuser.properties%}#${HADOOP_PROXY_USER_PROPERTIES}#g" `grep "{%hadoop.proxyuser.properties%}" -rl ${output_dir}`
+        sed -i "s#{%hadoop.proxyuser.properties%}#${HADOOP_PROXY_USER_PROPERTIES}#g" ${CORE_SITE_CONFIG}
     else
-        sed -i "s#{%hadoop.proxyuser.properties%}#""#g" `grep "{%hadoop.proxyuser.properties%}" -rl ${output_dir}`
+        sed -i "s#{%hadoop.proxyuser.properties%}#""#g" ${CORE_SITE_CONFIG}
     fi
+}
+
+function update_nfs_dump_dir() {
+    # set nfs gateway dump dir
+    data_disk_dir=$(get_first_data_disk_dir)
+    if [ -z "$data_disk_dir" ]; then
+        nfs_dump_dir="/tmp/.hdfs-nfs"
+    else
+        nfs_dump_dir="$data_disk_dir/tmp/.hdfs-nfs"
+    fi
+    sed -i "s!{%dfs.nfs3.dump.dir%}!${nfs_dump_dir}!g" ${HDFS_SITE_CONFIG}
 }
 
 function configure_hdfs() {
     prepare_base_conf
     mkdir -p ${HADOOP_HOME}/logs
 
+    CORE_SITE_CONFIG=${output_dir}/hadoop/core-site.xml
+    HDFS_SITE_CONFIG=${output_dir}/hadoop/hdfs-site.xml
+
     fs_default_dir="hdfs://${HEAD_ADDRESS}:9000"
-    sed -i "s!{%fs.default.name%}!${fs_default_dir}!g" `grep "{%fs.default.name%}" -rl ${output_dir}`
+    sed -i "s!{%fs.default.name%}!${fs_default_dir}!g" ${CORE_SITE_CONFIG}
 
     update_proxy_user_for_current_user
     update_hdfs_data_disks_config
+    update_nfs_dump_dir
 
     HDFS_CONF_DIR=${HADOOP_HOME}/etc/hdfs
     # copy the existing hadoop conf
     mkdir -p ${HDFS_CONF_DIR}
     cp -r  ${HADOOP_HOME}/etc/hadoop/* ${HDFS_CONF_DIR}/
     # override hdfs conf
-    cp ${output_dir}/hadoop/core-site.xml ${HDFS_CONF_DIR}/core-site.xml
-    cp ${output_dir}/hadoop/hdfs-site.xml  ${HDFS_CONF_DIR}/hdfs-site.xml
+    cp ${CORE_SITE_CONFIG} ${HDFS_CONF_DIR}/core-site.xml
+    cp ${HDFS_SITE_CONFIG} ${HDFS_CONF_DIR}/hdfs-site.xml
 
     # configure the default hadoop to HDFS, if there are other components configure
     # Hadoop, it will override this file
-    cp ${output_dir}/hadoop/core-site.xml ${HADOOP_HOME}/etc/hadoop/core-site.xml
+    cp ${CORE_SITE_CONFIG} ${HADOOP_HOME}/etc/hadoop/core-site.xml
+    cp ${HDFS_SITE_CONFIG} ${HADOOP_HOME}/etc/hadoop/hdfs-site.xml
 
     if [ $IS_HEAD_NODE == "true" ]; then
         # format only once if there is no force format flag
