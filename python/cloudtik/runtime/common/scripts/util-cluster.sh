2665400eebf600f725a31421f7ada1919a1cbc8d
Chen Haifeng <jerrychenhf@yahoo.com>
2023-12-07 17:15:30 +0800

    Runtime: reorganize the runtime common scripts (#95)

---
+++ python/cloudtik/runtime/common/scripts/util-cluster.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+
+function set_head_address() {
+    set_head_ip_address
+    # TODO: set the head host address when applicable
+}
+
+function set_head_ip_address() {
+    if [ -z "${HEAD_IP_ADDRESS}" ]; then
+        if [ $IS_HEAD_NODE == "true" ]; then
+            if [ ! -n "${CLOUDTIK_NODE_IP}" ]; then
+                HEAD_IP_ADDRESS=$(hostname -I | awk '{print $1}')
+            else
+                HEAD_IP_ADDRESS=${CLOUDTIK_NODE_IP}
+            fi
+        else
+            if [ ! -n "${CLOUDTIK_HEAD_IP}" ]; then
+                echo "Error: CLOUDTIK_HEAD_IP environment variable should be set."
+                exit 1
+            else
+                HEAD_IP_ADDRESS=${CLOUDTIK_HEAD_IP}
+            fi
+        fi
+    fi
+}
+
+function set_node_address() {
+    set_node_ip_address
+    # TODO: set the node host address when applicable
+}
+
+function set_node_ip_address() {
+    if [ -z "${NODE_IP_ADDRESS}" ]; then
+        if [ ! -n "${CLOUDTIK_NODE_IP}" ]; then
+            NODE_IP_ADDRESS=$(hostname -I | awk '{print $1}')
+        else
+            NODE_IP_ADDRESS=${CLOUDTIK_NODE_IP}
+        fi
+    fi
+}
+
+function set_head_option() {
+    # this function set the head variable based on the arguments processed by getopt
+    IS_HEAD_NODE=false
+    while true
+    do
+        case "$1" in
+        -h|--head)
+            IS_HEAD_NODE=true
+            ;;
+        --)
+            shift
+            break
+            ;;
+        esac
+        shift
+    done
+}
+
+function set_service_command() {
+    # this function set the SERVICE_COMMAND
+    # based on the arguments processed by getopt
+    while true
+    do
+        case "$1" in
+        --)
+            shift
+            break
+            ;;
+        esac
+        shift
+    done
+    SERVICE_COMMAND="$1"
+}
+
+function get_data_disk_dirs() {
+    local data_disk_dirs=""
+    if [ -d "/mnt/cloudtik" ]; then
+        for data_disk in /mnt/cloudtik/*; do
+            [ -d "$data_disk" ] || continue
+            if [ -z "$data_disk_dirs" ]; then
+                data_disk_dirs=$data_disk
+            else
+                data_disk_dirs="$data_disk_dirs,$data_disk"
+            fi
+        done
+    fi
+    echo "${data_disk_dirs}"
+}
+
+function get_first_data_disk_dir() {
+    local data_disk_dir=""
+    if [ -d "/mnt/cloudtik" ]; then
+        for data_disk in /mnt/cloudtik/*; do
+            [ -d "$data_disk" ] || continue
+            data_disk_dir=$data_disk
+            break
+        done
+    fi
+    echo "${data_disk_dir}"
+}
---
+++ python/cloudtik/runtime/common/scripts/util-os.sh
@@ -0,0 +1,123 @@
+#!/bin/bash
+# Copyright VMware, Inc.
+# SPDX-License-Identifier: APACHE-2.0
+#
+# Library for operating system actions
+
+# shellcheck disable=SC1091
+
+# Functions
+
+########################
+# Check if an user exists in the system
+# Arguments:
+#   $1 - user
+# Returns:
+#   Boolean
+#########################
+user_exists() {
+    local user="${1:?user is missing}"
+    id "$user" >/dev/null 2>&1
+}
+
+########################
+# Check if a group exists in the system
+# Arguments:
+#   $1 - group
+# Returns:
+#   Boolean
+#########################
+group_exists() {
+    local group="${1:?group is missing}"
+    getent group "$group" >/dev/null 2>&1
+}
+
+########################
+# Check if the script is currently running as root
+# Arguments:
+#   $1 - user
+#   $2 - group
+# Returns:
+#   Boolean
+#########################
+am_i_root() {
+    if [[ "$(id -u)" = "0" ]]; then
+        true
+    else
+        false
+    fi
+}
+
+########################
+# Get total memory available
+# Arguments:
+#   None
+# Returns:
+#   Memory in bytes
+#########################
+get_total_memory() {
+    echo $(($(grep MemTotal /proc/meminfo | awk '{print $2}') / 1024))
+}
+
+########################
+# Retries a command a given number of times
+# Arguments:
+#   $1 - cmd (as a string)
+#   $2 - max retries. Default: 12
+#   $3 - sleep between retries (in seconds). Default: 5
+# Returns:
+#   Boolean
+#########################
+retry_while() {
+    local cmd="${1:?cmd is missing}"
+    local retries="${2:-12}"
+    local sleep_time="${3:-5}"
+    local return_value=1
+
+    read -r -a command <<<"$cmd"
+    for ((i = 1; i <= retries; i += 1)); do
+        "${command[@]}" && return_value=0 && break
+        sleep "$sleep_time"
+    done
+    return $return_value
+}
+
+########################
+# Create md5 hash from a string
+# Arguments:
+#   $1 - string
+# Returns:
+#   md5 hash - string
+#########################
+generate_md5_hash() {
+    local -r str="${1:?missing input string}"
+    echo -n "$str" | md5sum | awk '{print $1}'
+}
+
+########################
+# Create sha1 hash from a string
+# Arguments:
+#   $1 - string
+#   $2 - algorithm - 1 (default), 224, 256, 384, 512
+# Returns:
+#   sha1 hash - string
+#########################
+generate_sha_hash() {
+    local -r str="${1:?missing input string}"
+    local -r algorithm="${2:-1}"
+    echo -n "$str" | "sha${algorithm}sum" | awk '{print $1}'
+}
+
+########################
+# Get the deb arch of the system
+# Returns:
+#   deb arch - string
+#########################
+function get_deb_arch() {
+    local deb_arch="amd64"
+    arch=$(uname -m)
+    if [ "${arch}" == "aarch64" ]; then
+        deb_arch="arm64"
+    fi
+    echo "${deb_arch}"
+}
