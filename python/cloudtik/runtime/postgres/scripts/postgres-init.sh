1d8015ddd47ec74f10129ed6dc0b966585e2bd13
Chen Haifeng <jerrychenhf@yahoo.com>
2024-01-04 21:39:49 +0800

    Runtime: Postgres primary restart and join as standby (#196)

--- python/cloudtik/runtime/postgres/scripts/postgres-init.sh
+++ python/cloudtik/runtime/postgres/scripts/postgres-init.sh
@@ -16,95 +16,83 @@ _is_sourced() {
 }
 
 _main() {
-	# if first arg looks like a flag, assume we want to run postgres server
-	if [ "${1:0:1}" = '-' ]; then
-		set -- postgres "$@"
-	fi
+  # if first arg looks like a flag, assume we want to run postgres server
+  if [ "${1:0:1}" = '-' ]; then
+    set -- postgres "$@"
+  fi
 
-	if [ "$1" = 'postgres' ] && ! _pg_want_help "$@"; then
-		postgres_setup_env
-		# setup data directories and permissions (when run as root)
-		postgres_create_db_directories
-		if [ "$(id -u)" = '0' ]; then
-			# then restart script as postgres user
-			exec gosu postgres "$BASH_SOURCE" "$@"
-		fi
+  if [ "$1" = 'postgres' ] && ! _pg_want_help "$@"; then
+    postgres_setup_env
+    # setup data directories and permissions (when run as root)
+    postgres_create_db_directories
+    if [ "$(id -u)" = '0' ]; then
+      # then restart script as postgres user
+      exec gosu postgres "$BASH_SOURCE" "$@"
+    fi
 
-		# only run initialization on an empty data directory
-		if [ -z "$DATABASE_ALREADY_EXISTS" ]; then
-			postgres_verify_minimum_env
+    if [ "${POSTGRES_ROLE}" == "primary" ]; then
+      # only run initialization on an empty data directory
+      if [ -z "$DATABASE_ALREADY_EXISTS" ]; then
+        postgres_verify_minimum_env
 
-			if [ ! -z "${POSTGRES_INITDB_SCRIPTS}" ]; then
-			  # check dir permissions to reduce likelihood of half-initialized database
-			  ls ${POSTGRES_INITDB_SCRIPTS}/ > /dev/null
-			fi
+        if [ ! -z "${POSTGRES_INITDB_SCRIPTS}" ]; then
+          # check dir permissions to reduce likelihood of half-initialized database
+          ls ${POSTGRES_INITDB_SCRIPTS}/ > /dev/null
+        fi
 
-			if [ "${POSTGRES_ROLE}" == "primary" ]; then
-				postgres_init_database_dir
-				pg_setup_hba_conf "$@"
-				if [ "${POSTGRES_REPMGR_ENABLED}" == "true" ]; then
-					repmgr_setup_hba_conf "$@"
-				fi
+        postgres_init_database_dir
+        pg_setup_hba_conf "$@"
+        if [ "${POSTGRES_REPMGR_ENABLED}" == "true" ]; then
+          repmgr_setup_hba_conf "$@"
+        fi
 
-				# PGPASSWORD is required for psql when authentication is required for 'local' connections via pg_hba.conf and is otherwise harmless
-				# e.g. when '--auth=md5' or '--auth-local=md5' is used in POSTGRES_INITDB_ARGS
-				export PGPASSWORD="${PGPASSWORD:-$POSTGRES_PASSWORD}"
-				postgres_temp_server_start "$@"
+        # PGPASSWORD is required for psql when authentication is required for 'local' connections via pg_hba.conf and is otherwise harmless
+        # e.g. when '--auth=md5' or '--auth-local=md5' is used in POSTGRES_INITDB_ARGS
+        export PGPASSWORD="${PGPASSWORD:-$POSTGRES_PASSWORD}"
+        postgres_temp_server_start "$@"
 
-				postgres_setup_db
-				postgres_setup_replication_user
+        postgres_setup_db
+        postgres_setup_replication_user
 
-				if [ "${POSTGRES_REPMGR_ENABLED}" == "true" ]; then
-					repmgr_create_repmgr_user
-					repmgr_create_repmgr_db
-				fi
+        if [ "${POSTGRES_REPMGR_ENABLED}" == "true" ]; then
+          repmgr_create_repmgr_user
+          repmgr_create_repmgr_db
+        fi
 
-				postgres_init_db_and_user
-				if [ ! -z "${POSTGRES_INITDB_SCRIPTS}" ]; then
-				  postgres_process_init_files ${POSTGRES_INITDB_SCRIPTS}/*
-				fi
+        postgres_init_db_and_user
+        if [ ! -z "${POSTGRES_INITDB_SCRIPTS}" ]; then
+          postgres_process_init_files ${POSTGRES_INITDB_SCRIPTS}/*
+        fi
 
-				postgres_temp_server_stop
-				unset PGPASSWORD
-			else
-				# for replica, we needs to do a pg_basebackup from master
-				# Cannot use an emtpy data directory or a data directory initialized
-				# by initdb (this method will make the data files with different identifier.
-				# This process will setup primary_conninfo in the postgres.auto.conf
-				# and the standby.signal in the data directory
-				export PGPASSWORD="${POSTGRES_REPLICATION_PASSWORD:-cloudtik}"
-				local replication_slot_options=""
-				if [ ! -z "$POSTGRES_REPLICATION_SLOT_NAME" ]; then
-					replication_slot_options="-C -S $POSTGRES_REPLICATION_SLOT_NAME"
-				fi
-				pg_basebackup -h ${POSTGRES_PRIMARY_HOST} \
-					-U repl_user --no-password ${replication_slot_options} \
-					-X stream -R -D $PGDATA
-				unset PGPASSWORD
-			fi
+        postgres_temp_server_stop
+        unset PGPASSWORD
 
-			cat <<-'EOM'
+        cat <<-'EOM'
 
-				PostgreSQL init process complete; ready for start up.
+          PostgreSQL init process complete; ready for start up.
 
-			EOM
-		else
-			cat <<-'EOM'
+        EOM
+      else
+        cat <<-'EOM'
 
-				PostgreSQL Database directory appears to contain a database; Skipping initialization
+          PostgreSQL Database directory appears to contain a database; Skipping initialization
 
-			EOM
-		fi
+        EOM
+      fi
+    else
+        # standby role (wait for primary node)
+        postgres_clone_primary
+    fi
 
-		if [ "${POSTGRES_REPMGR_ENABLED}" == "true" ]; then
-			repmgr_configure_preload
-		fi
-		# TODO: confirm whether synchronous standby works with repmgr
-		postgres_setup_synchronous_standby
-	fi
+    if [ "${POSTGRES_REPMGR_ENABLED}" == "true" ]; then
+      repmgr_configure_preload
+    fi
+    # TODO: confirm whether synchronous standby works with repmgr
+    postgres_setup_synchronous_standby
+  fi
 
   #  Use this as init script
-	# exec "$@"
+  # exec "$@"
 }
 
 if ! _is_sourced; then
--- python/cloudtik/runtime/postgres/scripts/postgres.sh
+++ python/cloudtik/runtime/postgres/scripts/postgres.sh
@@ -7,7 +7,9 @@ BIN_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 ROOT_DIR="$(dirname "$(dirname "$BIN_DIR")")"
 
 # Load generic functions
+. "$ROOT_DIR"/common/scripts/util-log.sh
 . "$ROOT_DIR"/common/scripts/util-file.sh
+. "$ROOT_DIR"/common/scripts/util-fs.sh
 
 ########################
 # Change a PostgreSQL configuration file by setting a property
@@ -482,4 +484,28 @@ postgres_get_waldir() {
         # Uninitialized - using value from $POSTGRES_INITDB_WAL_DIR if set
         echo "$POSTGRES_INITDB_WAL_DIR"
     fi
+}
+
+postgres_clone_primary(){
+    # for replica, we needs to do a pg_basebackup from master
+    # Cannot use an emtpy data directory or a data directory initialized
+    # by initdb (this method will make the data files with different identifier.
+    # This process will setup primary_conninfo in the postgres.auto.conf
+    # and the standby.signal in the data directory
+    # Clears WAL directory if existing (pg_basebackup requires the WAL dir to be empty)
+    local -r waldir=$(postgres_get_waldir)
+    if [[ -d "$waldir" ]]; then
+        info "Deleting existing WAL directory $waldir..."
+        rm -rf "$waldir" && ensure_dir_exists "$waldir"
+    fi
+
+    export PGPASSWORD="${POSTGRES_REPLICATION_PASSWORD:-cloudtik}"
+    local replication_slot_options=""
+    if [ ! -z "$POSTGRES_REPLICATION_SLOT_NAME" ]; then
+      replication_slot_options="-C -S $POSTGRES_REPLICATION_SLOT_NAME"
+    fi
+    pg_basebackup -h ${POSTGRES_PRIMARY_HOST} \
+      -U repl_user --no-password ${replication_slot_options} \
+      -X stream -R -D $PGDATA
+    unset PGPASSWORD
 }
