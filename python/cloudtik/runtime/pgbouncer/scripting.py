0275bf3031727aee8241139cb925652e84c16e0d
Chen Haifeng <haifeng.chen@intel.com>
2024-02-03 16:05:18 +0800

    Runtime: improve the PgBouncer auth

--- python/cloudtik/runtime/pgbouncer/scripting.py
+++ python/cloudtik/runtime/pgbouncer/scripting.py
@@ -3,9 +3,11 @@ import os
 from cloudtik.core._private.util.core_utils import open_with_mode
 from cloudtik.core._private.util.database_utils import get_database_username_with_default, \
     get_database_password_with_default, \
-    get_database_address, get_database_port, get_database_name, get_database_username
+    get_database_address, get_database_port, get_database_name
 from cloudtik.core._private.util.runtime_utils import get_runtime_config_from_node
-from cloudtik.runtime.pgbouncer.utils import _get_config, _get_home_dir, _get_backend_databases, _get_backend_config
+from cloudtik.runtime.pgbouncer.utils import _get_config, _get_home_dir, _get_backend_databases, _get_backend_config, \
+    _is_database_bind_user, _get_database_connect, _get_database_auth_user, \
+    _get_database_auth_password
 
 
 ###################################
@@ -26,11 +28,6 @@ def _get_auth_file():
     return os.path.join(_get_config_dir(), "userlist.txt")
 
 
-def _is_connect_bind_user(database_config):
-    username = get_database_username(database_config)
-    return True if username else False
-
-
 def configure_backend(head):
     runtime_config = get_runtime_config_from_node(head)
     pgbouncer_config = _get_config(runtime_config)
@@ -45,25 +42,57 @@ def configure_backend(head):
     _update_backends(backend_databases, username_password_conflicts)
 
 
+def _add_user_password(
+        username_password_map, username_password_conflicts,
+        username, password):
+    if not username:
+        return
+    if username in username_password_conflicts:
+        return
+    if username not in username_password_map:
+        username_password_map[username] = password
+    else:
+        existing_password = username_password_map[username]
+        if existing_password != password:
+            # conflicts
+            username_password_map.pop(username)
+            username_password_conflicts.add(username)
+
+
+def _add_connect_username_password(
+        username_password_map, username_password_conflicts,
+        database_connect):
+    if not database_connect:
+        return
+    username = get_database_username_with_default(database_connect)
+    password = get_database_password_with_default(database_connect)
+    _add_user_password(
+        username_password_map, username_password_conflicts,
+        username, password)
+
+
+def _add_auth_username_password(
+        username_password_map, username_password_conflicts,
+        database_config):
+    # WARNING: we should avoid auth user password conflicts
+    username = _get_database_auth_user(database_config)
+    password = _get_database_auth_password(database_config)
+    _add_user_password(
+        username_password_map, username_password_conflicts,
+        username, password)
+
+
 def _get_username_password_info(backend_databases):
     username_password_conflicts = set()
     username_password_map = {}
     for _, database_config in backend_databases.items():
-        if not _is_connect_bind_user(database_config):
-            continue
-        username = get_database_username_with_default(database_config)
-        if username in username_password_conflicts:
-            continue
-        password = get_database_password_with_default(database_config)
-        if username not in username_password_map:
-            username_password_map[username] = password
-        else:
-            existing_password = username_password_map[username]
-            if existing_password != password:
-                # conflicts
-                username_password_map.pop(username)
-                username_password_conflicts.add(username)
-
+        database_connect = _get_database_connect(database_config)
+        _add_connect_username_password(
+            username_password_map,  username_password_conflicts,
+            database_connect)
+        _add_auth_username_password(
+            username_password_map, username_password_conflicts,
+            database_config)
     return username_password_map, username_password_conflicts
 
 
@@ -99,27 +128,46 @@ def _get_backend_database_lines(
     for database_name, database_config in backend_databases.items():
         backend_database_line = _get_backend_database_line(
             database_name, database_config, username_password_conflicts)
-        backend_database_lines.append(backend_database_line)
+        if backend_database_line:
+            backend_database_lines.append(backend_database_line)
     return backend_database_lines
 
 
 def _get_backend_database_line(
         database_name, database_config, username_password_conflicts):
-    host = get_database_address(database_config)
-    port = get_database_port(database_config)
+    database_connect = _get_database_connect(database_config)
+    if not database_connect:
+        return None
+    host = get_database_address(database_connect)
+    if not host:
+        return None
+
+    port = get_database_port(database_connect)
     connect_str = f"host={host} port={port}"
 
-    db_name = get_database_name(database_config)
+    db_name = get_database_name(database_connect)
     if db_name:
         connect_str += f" dbname={db_name}"
 
-    if _is_connect_bind_user(database_config):
-        username = get_database_username_with_default(database_config)
+    if _is_database_bind_user(database_config):
+        username = get_database_username_with_default(database_connect)
         connect_str += f" user={username}"
         if username in username_password_conflicts:
             # need use password in the connect line
-            password = get_database_password_with_default(database_config)
+            password = get_database_password_with_default(database_connect)
             connect_str += f" password={password}"
+
+    auth_user = _get_database_auth_user(database_config)
+    if auth_user:
+        connect_str += f" auth_user={auth_user}"
+
+    # Only available on 1.22.0 upwards
+    """
+    auth_query = _get_database_auth_query(database_config)
+    if auth_query:
+        # Simple query such as: SELECT usename, passwd FROM pg_shadow WHERE usename=$1
+        connect_str += f" auth_query='{auth_query}'"
+    """
 
     connect_line = "{} = {}".format(database_name, connect_str)
     return connect_line
