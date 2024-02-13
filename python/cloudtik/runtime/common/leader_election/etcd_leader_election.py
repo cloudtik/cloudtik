3509cba55f71717fc970fb71a9ef5b78d55bc916
Chen Haifeng <jerrychenhf@yahoo.com>
2024-02-13 17:21:35 +0800

    Runtime: implement ETCD based distributed lock and leader election (#280)

---
+++ python/cloudtik/runtime/common/leader_election/etcd_leader_election.py
@@ -0,0 +1,165 @@
+from datetime import datetime
+
+from cloudtik.runtime.common.etcd_utils import \
+    create_session, acquire_key, destroy_session, renew_session, \
+    get_key, query_key_blocking
+from cloudtik.runtime.common.leader_election.leader_election_base import LeaderElection
+
+# the key will always be substituted into this pattern
+# a good prefix is recommended for organization
+FULL_KEY_PATTERN = 'cloudtik/leader/%s'
+
+
+class EtcdLeaderElection(LeaderElection):
+    def __init__(
+            self,
+            key,
+            leader_ttl=None,
+            leader_elect_delay=None):
+        """
+        Args:
+        key: the unique key to lock
+        leader_ttl: how long the lock will stay alive if it is never released,
+            this is controlled by Etcd lease TTL and may stay alive a bit longer according
+            to their docs.
+        """
+        super().__init__(key, leader_ttl, leader_elect_delay)
+        self.full_key = FULL_KEY_PATTERN % key
+        self.session_id = None
+        self.leader = False
+
+    def elect(self) -> bool:
+        """
+        Attempt to acquire the lock for leader
+
+        return: True if the lock was successfully acquired,
+            false if it was not (unreachable if failing hard)
+        """
+        assert not self.leader, 'Already elect as leader.'
+
+        session = self._create_session()
+        self.session_id = session["ID"]
+        self.leader = self._acquire_key()
+        if not self.leader:
+            # failed to become leader
+            self._destroy_session()
+        return self.leader
+
+    def step_down(self):
+        """
+        Release the lock immediately. Does nothing if never locked.
+        """
+        if not self.leader:
+            return False
+        return self._release_key()
+
+    def get_current_leader(self):
+        """
+        Query the key for current leader
+        """
+        resp = get_key(self.full_key)
+        if not resp:
+            # no leader
+            return None
+
+        key_meta = self._get_key_meta(resp)
+        if not key_meta:
+            return None
+
+        global_index = self._get_global_revision(resp)
+        if global_index:
+            key_meta["global_revision"] = global_index
+        return key_meta
+
+    def is_leader(self, current_leader):
+        """
+        Check whether this instance is the leader. Return True if it is
+        the current leader
+        """
+        if not self.session_id:
+            return False
+        if not current_leader:
+            return False
+        session_id = self._get_session_id(current_leader)
+        if self.session_id != session_id:
+            return False
+        return True
+
+    def watch(self, current_leader):
+        """
+        Watch the changes of the current leader. This may be called in a
+        separate thread than the main methods.
+        """
+        if not current_leader:
+            return
+        # watch use global revision for watch if exists
+        revision = self._get_key_revision(current_leader)
+        query_key_blocking(self.full_key, revision)
+
+    def heartbeat(self, current_leader):
+        """
+        Heartbeat to the session for keep the current leader alive. This
+        may be called in a separate thread than the main methods.
+        """
+        if not current_leader:
+            return
+        session_id = self._get_session_id(current_leader)
+        renew_session(session_id)
+
+    def _create_session(self):
+        # how long to keep the session alive without a renew (heartbeat/keepalive) sent.
+        # we are using this to time out the individual lock
+        session_ttl = self.leader_ttl
+        session = create_session(
+            ttl=session_ttl
+        )
+        return session
+
+    def _destroy_session(self):
+        if not self.session_id:
+            return False
+        result = destroy_session(session_id=self.session_id)
+        self.session_id = None
+        self.leader = False
+        return result
+
+    def _acquire_key(self):
+        assert self.session_id, 'Must have a session id to acquire key'
+        data = str(datetime.now())
+        return acquire_key(
+            session_id=self.session_id,
+            key=self.full_key,
+            value=data,
+        )
+
+    def _release_key(self):
+        # destroying the session will is the safest way to release the lock. we'd like to delete the
+        # key, but since it's possible we don't actually have the lock anymore (in distributed systems,
+        # there is no spoon). It's best to just destroy the session and let the lock get cleaned up by
+        # Etcd.
+        return self._destroy_session()
+
+    def _get_key_meta(self, resp):
+        keys = resp.get("kvs")
+        if not keys:
+            return None
+        key_meta = keys[0]
+        session_id = self._get_session_id(key_meta)
+        if not session_id:
+            return None
+        return key_meta
+
+    @staticmethod
+    def _get_global_revision(resp):
+        return resp.get("header", {}).get("revision")
+
+    @staticmethod
+    def _get_key_revision(key_meta):
+        global_revision = key_meta.get("global_revision")
+        if global_revision:
+            return global_revision
+        return key_meta.get("mod_revision")
+
+    @staticmethod
+    def _get_session_id(key_meta):
+        return key_meta.get("lease")
---
+++ python/cloudtik/runtime/common/lock/etcd_lock.py
@@ -0,0 +1,114 @@
+import time
+from datetime import datetime
+
+from cloudtik.runtime.common.etcd_utils import destroy_session, acquire_key, create_session
+from cloudtik.runtime.common.lock.lock_base import Lock, LOCK_MAX_ATTEMPTS, LockAcquisitionException
+
+# the key will always be substituted into this pattern before locking,
+# a good prefix is recommended for organization
+FULL_KEY_PATTERN = 'cloudtik/locks/%s'
+
+"""
+Each lease has a minimum time-to-live (TTL) value specified by the application at grant time.
+The lease’s actual TTL value is at least the minimum TTL and is chosen by the etcd cluster.
+Once a lease’s TTL elapses, the lease expires and all attached keys are deleted.
+"""
+
+
+class EtcdLock(Lock):
+    def __init__(
+            self,
+            key,
+            lock_timeout_seconds=None,
+            acquire_timeout_ms=None):
+        """
+        Args:
+        key: the unique key to lock
+        acquire_timeout_ms: how long the caller is willing to wait to acquire the lock
+        lock_timeout_seconds: how long the lock will stay alive if it is never released,
+            this is controlled by Etcd lease TTL and may stay alive a bit longer according
+            to their docs.
+        """
+        super().__init__(key, acquire_timeout_ms, lock_timeout_seconds)
+        self.full_key = FULL_KEY_PATTERN % key
+        self.session_id = None
+        self._started_acquiring = False
+
+    def acquire(self, fail_hard=True):
+        """
+        Attempt to acquire the lock.
+
+        Args:
+        fail_hard: when true, this method will only return gracefully
+            if the lock has been acquired and will throw an exception if
+            it cannot acquire the lock.
+
+        return: True if the lock was successfully acquired,
+            false if it was not (unreachable if failing hard)
+        """
+        assert not self._started_acquiring, 'Can only lock once'
+        start_time = time.time()
+
+        session = self._create_session()
+        self.session_id = session["ID"]
+        self._started_acquiring = True
+
+        acquired = False
+        max_attempts = LOCK_MAX_ATTEMPTS  # don't loop forever
+        for attempt in range(0, max_attempts):
+            acquired = self._acquire_key()
+
+            # exponential backoff yo
+            sleep_ms = 50 * pow(attempt, 2)
+            elapsed_time_ms = int(round(1000 * (time.time() - start_time)))
+            time_left_ms = self.acquire_timeout_ms - elapsed_time_ms
+            sleep_ms = min(time_left_ms, sleep_ms)
+
+            retry_acquire = (not acquired) and (time_left_ms > 0)
+            if retry_acquire:
+                sleep_seconds_float = sleep_ms / 1000.0
+                time.sleep(sleep_seconds_float)
+            else:
+                break
+
+        if not acquired and fail_hard:
+            raise LockAcquisitionException("Failed to acquire %s" % self.full_key)
+        else:
+            return acquired
+
+    def release(self):
+        """
+        Release the lock immediately. Does nothing if never locked.
+        """
+        if not self._started_acquiring:
+            return False
+        return self._release_key()
+
+    def _create_session(self):
+        # how long to keep the session alive without a renew (heartbeat/keepalive) sent.
+        # we are using this to time out the individual lock
+        session_ttl = self.lock_timeout_seconds
+        session = create_session(
+            ttl=session_ttl
+        )
+        return session
+
+    def _destroy_session(self):
+        assert self.session_id, 'Must have a session id to destroy'
+        return destroy_session(session_id=self.session_id)
+
+    def _acquire_key(self):
+        assert self.session_id, 'Must have a session id to acquire key'
+        data = str(datetime.now())
+        return acquire_key(
+            session_id=self.session_id,
+            key=self.full_key,
+            value=data,
+        )
+
+    def _release_key(self):
+        # destroying the session will is the safest way to release the lock. we'd like to delete the
+        # key, but since it's possible we don't actually have the lock anymore (in distributed systems,
+        # there is no spoon). It's best to just destroy the session and let the lock get cleaned up by
+        # Etcd.
+        return self._destroy_session()
